4bca3a940ea975350c22264b687d51c2b84bf264
Martin Hammerchmidt <martin.hammerchmidt@gmail.com>
2015-04-22 20:10:30 +0200

    Window is now resizable

--- src/application.cpp
+++ src/application.cpp
@@ -12,7 +12,7 @@
 const sf::Time Application::TimePerFrame = sf::seconds(1.f/60.f);
 
 Application::Application() :
-    mWindow(sf::VideoMode(1024, 576), "Redhead against Rabbit", sf::Style::Close),
+    mWindow(sf::VideoMode(1024, 576), "Redhead against Rabbit"),
     mStateStack(State::Context(mWindow, mTextures, mFonts, mPlayer)),
     mStatisticsNumFrames(0)
 {
